c4ed614939f72807ec918a08ae719333885e7f8b
Anton Bukov <k06aaa@gmail.com>
2018-08-01 02:49:38 +0300

    Some formatting

--- contracts/EC.sol
+++ contracts/EC.sol
@@ -1,5 +1,8 @@
-pragma solidity ^0.4.2;
+pragma solidity ^0.4.24;
 
+//
+// https://github.com/jbaylina/ecsol
+//
 contract EC {
 
     uint256 constant public gx = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798;
@@ -8,54 +11,66 @@ contract EC {
     uint256 constant public a = 0;
     uint256 constant public b = 7;
 
-    function _jAdd( uint256 x1,uint256 z1,
-                    uint256 x2,uint256 z2) public pure
-        returns(uint256 x3,uint256 z3)
+    function _jAdd(uint256 x1, uint256 z1,
+                   uint256 x2, uint256 z2) public pure
+        returns(uint256 x3, uint256 z3)
     {
-        (x3, z3) = (  addmod( mulmod(z2, x1 , n) ,
-                              mulmod(x2, z1 , n),
-                              n),
-                      mulmod(z1, z2 , n)
-                    );
+        (x3, z3) = (
+            addmod(
+                mulmod(z2, x1, n),
+                mulmod(x2, z1, n),
+                n
+            ),
+            mulmod(z1, z2, n)
+        );
     }
 
-    function _jSub( uint256 x1,uint256 z1,
-                    uint256 x2,uint256 z2) public pure
-        returns(uint256 x3,uint256 z3)
+    function _jSub(uint256 x1, uint256 z1,
+                   uint256 x2, uint256 z2) public pure
+        returns(uint256 x3, uint256 z3)
     {
-        (x3, z3) = (  addmod( mulmod(z2, x1, n),
-                              mulmod(n - x2, z1, n),
-                              n),
-                      mulmod(z1, z2 , n)
-                    );
+        (x3, z3) = (
+            addmod(
+                mulmod(z2, x1, n),
+                mulmod(n - x2, z1, n),
+                n
+            ),
+            mulmod(z1, z2, n)
+        );
     }
 
-    function _jMul( uint256 x1,uint256 z1,
-                    uint256 x2,uint256 z2) public pure
-        returns(uint256 x3,uint256 z3)
+    function _jMul(uint256 x1, uint256 z1,
+                   uint256 x2, uint256 z2) public pure
+        returns(uint256 x3, uint256 z3)
     {
-        (x3, z3) = (  mulmod(x1, x2 , n), mulmod(z1, z2 , n));
+        (x3, z3) = (
+            mulmod(x1, x2, n),
+            mulmod(z1, z2, n)
+        );
     }
 
-    function _jDiv( uint256 x1,uint256 z1,
-                    uint256 x2,uint256 z2) public pure
-        returns(uint256 x3,uint256 z3)
+    function _jDiv(uint256 x1, uint256 z1,
+                   uint256 x2, uint256 z2) public pure
+        returns(uint256 x3, uint256 z3)
     {
-        (x3, z3) = (  mulmod(x1, z2 , n), mulmod(z1 , x2 , n));
+        (x3, z3) = (
+            mulmod(x1, z2, n),
+            mulmod(z1, x2, n)
+        );
     }
 
-    function _inverse( uint256 val) public pure
+    function _inverse(uint256 val) public pure
         returns(uint256 invVal)
     {
-        uint256 t=0;
-        uint256 newT=1;
-        uint256 r=n;
-        uint256 newR=val;
+        uint256 t = 0;
+        uint256 newT = 1;
+        uint256 r = n;
+        uint256 newR = val;
         uint256 q;
         while (newR != 0) {
             q = r / newR;
 
-            (t, newT) = (newT, addmod(t , (n - mulmod(q, newT,n)) , n));
+            (t, newT) = (newT, addmod(t, (n - mulmod(q, newT, n)), n));
             (r, newR) = (newR, r - q * newR );
         }
 
@@ -63,45 +78,43 @@ contract EC {
     }
 
 
-    function _ecAdd(uint256 x1,uint256 y1,uint256 z1,
-                    uint256 x2,uint256 y2,uint256 z2) public pure
-        returns(uint256 x3,uint256 y3,uint256 z3)
+    function _ecAdd(uint256 x1, uint256 y1, uint256 z1,
+                    uint256 x2, uint256 y2, uint256 z2) public pure
+        returns(uint256 x3, uint256 y3, uint256 z3)
     {
         uint256 l;
         uint256 lz;
         uint256 da;
         uint256 db;
 
-        if ((x1==0)&&(y1==0)) {
-            return (x2,y2,z2);
+        if (x1 == 0 && y1 == 0) {
+            return (x2, y2, z2);
         }
 
-        if ((x2==0)&&(y2==0)) {
-            return (x1,y1,z1);
+        if (x2 == 0 && y2 == 0) {
+            return (x1, y1, z1);
         }
 
-        if ((x1==x2)&&(y1==y2)) {
-            (l,lz) = _jMul(x1, z1, x1, z1);
-            (l,lz) = _jMul(l, lz, 3, 1);
-            (l,lz) = _jAdd(l, lz, a, 1);
+        if (x1 == x2 && y1 == y2) {
+            (l, lz) = _jMul(x1, z1, x1, z1);
+            (l, lz) = _jMul(l, lz, 3, 1);
+            (l, lz) = _jAdd(l, lz, a, 1);
 
             (da,db) = _jMul(y1, z1, 2, 1);
         } else {
-            (l,lz) = _jSub(y2, z2, y1, z1);
-            (da,db)  = _jSub(x2, z2, x1, z1);
+            (l, lz) = _jSub(y2, z2, y1, z1);
+            (da, db) = _jSub(x2, z2, x1, z1);
         }
 
         (l, lz) = _jDiv(l, lz, da, db);
-
 
         (x3, da) = _jMul(l, lz, l, lz);
         (x3, da) = _jSub(x3, da, x1, z1);
         (x3, da) = _jSub(x3, da, x2, z2);
 
         (y3, db) = _jSub(x1, z1, x3, da);
-        (y3, db) = _jMul(y3, db, l, lz );
-        (y3, db) = _jSub(y3, db, y1, z1 );
-
+        (y3, db) = _jMul(y3, db, l, lz);
+        (y3, db) = _jSub(y3, db, y1, z1);
 
         if (da != db) {
             x3 = mulmod(x3, db, n);
@@ -110,17 +123,16 @@ contract EC {
         } else {
             z3 = da;
         }
-
     }
 
-    function _ecDouble(uint256 x1,uint256 y1,uint256 z1) public pure
-        returns(uint256 x3,uint256 y3,uint256 z3)
+    function _ecDouble(uint256 x1, uint256 y1, uint256 z1) public pure
+        returns(uint256 x3, uint256 y3, uint256 z3)
     {
-        (x3,y3,z3) = _ecAdd(x1,y1,z1,x1,y1,z1);
+        (x3, y3, z3) = _ecAdd(x1, y1, z1, x1, y1, z1);
     }
 
-    function _ecMul(uint256 d, uint256 x1,uint256 y1,uint256 z1) public pure
-        returns(uint256 x3,uint256 y3,uint256 z3)
+    function _ecMul(uint256 d, uint256 x1, uint256 y1, uint256 z1) public pure
+        returns(uint256 x3, uint256 y3, uint256 z3)
     {
         uint256 remaining = d;
         uint256 px = x1;
@@ -130,19 +142,19 @@ contract EC {
         uint256 acy = 0;
         uint256 acz = 1;
 
-        if (d==0) {
-            return (0,0,1);
+        if (d == 0) {
+            return (0, 0, 1);
         }
 
         while (remaining != 0) {
             if ((remaining & 1) != 0) {
-                (acx,acy,acz) = _ecAdd(acx,acy,acz, px,py,pz);
+                (acx,acy,acz) = _ecAdd(acx, acy, acz, px, py, pz);
             }
             remaining = remaining / 2;
-            (px,py,pz) = _ecDouble(px,py,pz);
+            (px, py, pz) = _ecDouble(px, py, pz);
         }
 
-        (x3,y3,z3) = (acx,acy,acz);
+        (x3, y3, z3) = (acx, acy, acz);
     }
 
     function ecadd(uint256 x1, uint256 y1,
@@ -196,5 +208,4 @@ contract EC {
         qx = mulmod(qx, z, n);
         qy = mulmod(qy, z, n);
     }
-
 }
